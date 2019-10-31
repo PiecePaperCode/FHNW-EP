34934789a5c46b384226e5d70ca3c530106d0de1
PiecePaperCode <40020346+PiecePaperCode@users.noreply.github.com>
2019-10-31 19:37:14 +0100

    Korrigiert Filehandler Windows cmd Linux terminal & Jetbrains
    Filehandler findet immer den richtigen Pfad

--- GUI Project/Filehandler.py
+++ GUI Project/Filehandler.py
@@ -1,7 +1,12 @@
+import os
+from pathlib import Path
+
+Basisroutine = Path(os.path.dirname(__file__)) / 'Basisroutine.txt'
+
 def read_Basisroutine():
-    with open('Basisroutine.txt') as file:
+    with open(Basisroutine) as file:
         return file.read()
 
 def write_Basisroutine(text):
-    with open('Basisroutine.txt', 'w') as file:
+    with open(Basisroutine, 'w') as file:
         file.write(text)
